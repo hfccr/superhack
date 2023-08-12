185dc54e1e87003585ecc5b524fdaad9ff1240e3
rb-au <raj@aureusanalytics.com>
2023-08-12 23:57:12 +0530

    Add fairness hook

--- packages/nextjs/components/search/NftInfo.tsx
+++ packages/nextjs/components/search/NftInfo.tsx
@@ -1,5 +1,6 @@
 import { Address } from "../scaffold-eth";
 import { useNFT } from "@zoralabs/nft-hooks";
+import { useNftFairness } from "~~/hooks/scaffold-eth/useNftFairness";
 
 interface IProps {
   nftToken: string;
@@ -10,6 +11,12 @@ export const NftInfo = ({ nftToken, nftTokenId }: IProps) => {
   const src = `https://embed.zora.co/${nftToken}/${nftTokenId}?title=false&controls=false&loop=false&autoplay=false`;
   const title = data?.metadata?.name ? data?.metadata?.name : data?.nft?.contract?.name;
   const owner = data?.nft?.owner?.address;
+  const {
+    fetching: fairnessFetching,
+    success: fairnessSuccess,
+    error: fairnessError,
+    data: fairness,
+  } = useNftFairness(nftToken, nftTokenId);
   return (
     <>
       <div className="flex flex-col mt-6 px-7 py-8 bg-base-200 opacity-80 ">
@@ -32,7 +39,26 @@ export const NftInfo = ({ nftToken, nftTokenId }: IProps) => {
             <p>{data?.metadata?.description}</p>
             {owner && <Address address={owner} />}
             <div className="card-actions justify-end">
-              <button className="btn btn-primary">Listen</button>
+              {fairnessFetching && <span className="loading loading-spinner loading-xs"></span>}
+              {fairnessError && (
+                <div className="alert alert-error">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="stroke-current shrink-0 h-6 w-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                    />
+                  </svg>
+                  <span>Error! Failed to fetch fairness score</span>
+                </div>
+              )}
+              {fairnessSuccess && <button className="btn btn-primary">{fairness?.score}</button>}
             </div>
           </div>
         </div>
---
+++ packages/nextjs/hooks/scaffold-eth/useNftFairness.ts
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+
+interface ITransactions {
+  interacted_with: string;
+  tokenId: string;
+  blockTimestamp: string;
+}
+
+interface IFairness {
+  score: number;
+  fairTransactions: ITransactions;
+  unfairTransactions: ITransactions;
+  unknownTransactions: ITransactions;
+}
+
+interface IRemoteFairness {
+  fetching: boolean;
+  success: boolean;
+  error: boolean;
+  data: IFairness | undefined;
+}
+
+const PATH = "https://superhack-nft-fairness-production.up.railway.app/rest/classifier/score?";
+
+export function useNftFairness(nftToken: string, nftTokenId: string) {
+  const [fairness, setFairness] = useState<IRemoteFairness>({
+    fetching: false,
+    error: false,
+    success: false,
+    data: undefined,
+  });
+  useEffect(() => {
+    const getFairness = async () => {
+      setFairness({
+        fetching: true,
+        success: false,
+        error: false,
+        data: undefined,
+      });
+      try {
+        const URI =
+          PATH +
+          new URLSearchParams({
+            contractAddress: nftToken,
+            tokenIds: nftTokenId,
+          });
+        const response = await fetch(URI, {
+          cache: "no-store",
+        });
+        const responseObject: IFairness = await response.json();
+        setFairness({
+          fetching: false,
+          success: true,
+          error: false,
+          data: responseObject,
+        });
+      } catch (error) {
+        setFairness({
+          fetching: false,
+          success: false,
+          error: true,
+          data: undefined,
+        });
+      }
+    };
+    getFairness();
+  }, [nftToken, nftTokenId]);
+  return fairness;
+}
